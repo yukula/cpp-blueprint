977068cea132ac841e5695eeecb72d4de08884af
Uladzislau Yukhno <uladzislau.yukhno@gmail.com>
2024-05-24 21:47:10 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "c++ dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell.override { stdenv = llvmPackages_18.stdenv; } {
+            buildInputs = [ clang-tools_18 ];
+          };
+        });
+
+}
